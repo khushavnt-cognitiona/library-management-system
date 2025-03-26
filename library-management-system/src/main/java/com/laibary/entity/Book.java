3c05663d46ff24575c4e9e2e4ec903cd003c2ad2
Khushvant Jadhao <khusujadhao329@gmail.com>
2025-03-26 09:33:23 +0530

    Added constructors, getters, and setters in Book & Category entities

--- library-management-system/src/main/java/com/laibary/entity/Book.java
+++ library-management-system/src/main/java/com/laibary/entity/Book.java
@@ -20,4 +20,66 @@ public class Book {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
+    // 🔹 Default Constructor
+    public Book() {}
+
+    // 🔹 Parameterized Constructor
+    public Book(String title, String isbn, int copiesAvailable, Author author, Category category) {
+        this.title = title;
+        this.isbn = isbn;
+        this.copiesAvailable = copiesAvailable;
+        this.author = author;
+        this.category = category;
+    }
+
+    // 🔹 Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public int getCopiesAvailable() {
+        return copiesAvailable;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    // 🔹 Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setCopiesAvailable(int copiesAvailable) {
+        this.copiesAvailable = copiesAvailable;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
--- library-management-system/src/main/java/com/laibary/entity/Category.java
+++ library-management-system/src/main/java/com/laibary/entity/Category.java
@@ -10,4 +10,30 @@ public class Category {
     private Long id;
     
     private String name;
+
+    // 🔹 Default Constructor
+    public Category() {}
+
+    // 🔹 Parameterized Constructor
+    public Category(String name) {
+        this.name = name;
+    }
+
+    // 🔹 Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // 🔹 Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
