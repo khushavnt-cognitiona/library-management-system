4e6eba9708f1266f7857ef07aca7c0cc639f6e0a
Khushvant Jadhao <khusujadhao329@gmail.com>
2025-03-26 09:29:52 +0530

    Added constructors and getters/setters in Author entity

--- library-management-system/src/main/java/com/laibary/entity/Author.java
+++ library-management-system/src/main/java/com/laibary/entity/Author.java
@@ -5,9 +5,36 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "authors")
 public class Author {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     private String name;
+
+    // 🔹 Default Constructor (Required for JPA)
+    public Author() {}
+
+    // 🔹 Parameterized Constructor
+    public Author(String name) {
+        this.name = name;
+    }
+
+    // 🔹 Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // 🔹 Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
